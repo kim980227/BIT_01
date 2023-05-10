2c96594a8c254602f5e5793f8e7157a1a41af6c8
21700340JuwonBaek <root@DESKTOP-2TH44RN.localdomain>
2023-05-10 21:10:27 +0900

    run내의 코드 메소드화

--- src/threadServer/ServerDataReceiveThread.java
+++ src/threadServer/ServerDataReceiveThread.java
@@ -15,85 +15,97 @@ class ServerDataReceiveThread extends Thread{
 
     ThreadLocal<Boolean> identify = new ThreadLocal<Boolean>();
     Chat_User user = null;
+    InputStream is = null;
+    ObjectInputStream ois = null;
 
     public ServerDataReceiveThread(Socket socket) {
         // TODO Auto-generated constructor stub
         this.socket = socket;
     }
 
+    public void IOExceptionHandler(Exception e){
+        System.out.println("클라이언트와의 접속이 종료");
+        socketList = ServerConnectThread.getSocketList();
+        socketList.remove(socket);
+        System.out.println("현재인원: "+ socketList.size());
+        if((ServerConnectThread.leader == socket) && (ServerConnectThread.socketList.size() >= 1)){
+            ServerConnectThread.leader = ServerConnectThread.socketList.keySet().stream().findFirst().get();
+            System.out.println("방장변경...");
+            System.out.println(ServerConnectThread.leader);
+            ServerConnectThread.leaderChanged=true;
+        }
+        e.printStackTrace();
+
+        try {
+            is.close();
+            socket.close();
+            this.interrupt();
+
+        } catch (Exception e3) {
+            // TODO Auto-generated catch block
+            e3.printStackTrace();
+        }
+    }
+
+
+    public void sendToAll(Message message){
+        socketList = ServerConnectThread.getSocketList();
+        message.setUser(user);
+        message.setMsg(message.getUser().getName() + ": "+message.getMsg());
+        System.out.println("이름: "+message.getUser().getName());
+        System.out.println("메시지:" + message.getMsg());
+        //Write
+        for (Socket currentSocket: socketList.keySet()) {
+            try {
+                socketList.get(currentSocket).writeObject(message);
+            } catch (IOException e) {
+                IOExceptionHandler(e);
+            }
+            System.out.println("Writing data Finish: ");
+        }
+    }
+
+    public void checkIdentify(Message message){
+        if(!identify.get() ){
+            user = message.getUser();
+            ServerConnectThread.name_socket_mapper.put(user.getName(), socket);
+            if (ServerConnectThread.leader == socket){
+                user.setName(user.getName()+"(방장)");
+            }
+            identify.set(true);
+            ServerConnectThread.leaderChanged=false;
+        }
+    }
+
+    public void checkLeader(){
+        if(ServerConnectThread.leaderChanged){
+            if (ServerConnectThread.leader == socket){
+                user.setName(user.getName()+"(방장)");
+            }
+            ServerConnectThread.leaderChanged=false;
+        }
+    }
+
+
     public void run() {
-        InputStream is = null;
-        OutputStream os = null;
-        ObjectInputStream ois = null;
-        ObjectOutputStream oos = null;
         identify.set(false);
         try {
-            is = socket.getInputStream();
-            ois = new ObjectInputStream(is);
-
+            ois = new ObjectInputStream(socket.getInputStream());
             while(true) {
                 //Read
                 Message message  = (Message) ois.readObject();//Blocking 함수.
                 System.out.println("작성자: "+ message.getUser().getName() + " 내용: "+ message.getMsg());
-                if(!identify.get() ){
-                    user = message.getUser();
-                    ServerConnectThread.name_socket_mapper.put(user.getName(), socket);
-                    if (ServerConnectThread.leader == socket){
-                        user.setName(user.getName()+"(방장)");
-                    }
-                    identify.set(true);
-                    ServerConnectThread.leaderChanged=false;
-                }
-
-                if(ServerConnectThread.leaderChanged){
-                    if (ServerConnectThread.leader == socket){
-                        user.setName(user.getName()+"(방장)");
-                    }
-                    ServerConnectThread.leaderChanged=false;
-                }
+                checkIdentify(message);
+                checkLeader();
+                sendToAll(message);
                 //Result를 모든 socket에게 전송하기.
-                socketList = ServerConnectThread.getSocketList();
-                message.setUser(user);
-                message.setMsg(message.getUser().getName() + ": "+message.getMsg());
-                System.out.println("이름: "+message.getUser().getName());
-                System.out.println("메시지:" + message.getMsg());
-                //Write
-                for (Socket currentSocket: socketList.keySet()) {
-//                    os = currentSocket.getOutputStream();
-//                    oos = new ObjectOutputStream(os);
-                    socketList.get(currentSocket).writeObject(message);
-//                    oos.writeObject(message);
-                    System.out.println("Writing data Finish: ");
-                    // os.close();
-
-                }
             }
         } catch (ClassNotFoundException e1) {
             System.out.println("메시지 객체 타입 오류");
             e1.printStackTrace();
         }
         catch(IOException e2){
-            System.out.println("클라이언트와의 접속이 종료");
-            socketList = ServerConnectThread.getSocketList();
-            socketList.remove(socket);
-            System.out.println("현재인원: "+ socketList.size());
-            if((ServerConnectThread.leader == socket) && (ServerConnectThread.socketList.size() >= 1)){
-                ServerConnectThread.leader = ServerConnectThread.socketList.keySet().stream().findFirst().get();
-                System.out.println("방장변경...");
-                System.out.println(ServerConnectThread.leader);
-                ServerConnectThread.leaderChanged=true;
-            }
-            e2.printStackTrace();
-
-            try {
-                is.close();
-                socket.close();
-                this.interrupt();
-
-            } catch (Exception e3) {
-                // TODO Auto-generated catch block
-                e3.printStackTrace();
-            }
+            IOExceptionHandler(e2);
         }
     }
 }
